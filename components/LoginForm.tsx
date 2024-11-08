ecc1798bddcba42d8353b6dfae9a248f6be293e3
Keegan Botha <keegan@owlbox.com>
2024-11-08 13:38:24 +0200

    Initial commit

---
+++ components/LoginForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+export default function LoginForm() {
+  const [userName, setUserName] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  return (
+    <Card className="w-[30rem]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Fill in your credentials below to log in
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-4">
+          <Input />
+          <Input />
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+}
