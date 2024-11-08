23be05c361c16497f6b7a66150ece9e83340b316
Keegan Botha <keegan@owlbox.com>
2024-11-08 13:58:10 +0200

    Set up the basics for Auth Js, i dont fully understand it though

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screensss`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
       >
         {children}
       </body>
