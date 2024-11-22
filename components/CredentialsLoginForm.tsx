175d849d88343fd5e0e95e73b93787df6fa7dc00
Keegan Botha <keegan@owlbox.com>
2024-11-22 14:10:17 +0200

    changed the form name to track the login types

--- components/CredentialsLoginForm.tsx
+++ components/CredentialsLoginForm.tsx
@@ -14,7 +14,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 
-export default function LoginForm() {
+export default function CredentialsLoginForm() {
   return (
     <Card className="w-[25rem]">
       <form
